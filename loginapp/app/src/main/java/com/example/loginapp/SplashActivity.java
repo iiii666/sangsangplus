2a4e93a01423ed28ce73f4f329149fcd22357e4c
minzzung <dullini0205@gmail.com>
2021-09-02 19:31:16 +0900

    ㅁㄴㅇ

--- loginapp/app/src/main/java/com/example/loginapp/SplashActivity.java
+++ loginapp/app/src/main/java/com/example/loginapp/SplashActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
-
+    //변화
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
